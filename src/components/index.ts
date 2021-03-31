a6c22d48c06904c0618edd86e494f018dabdbfd9
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-01 00:03:48 +0200

    Add Help Comp

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Board from "./Board/Board";
 import Scores from "./Scores/Scores"
 import Tile from "./Board/Tile/Tile"
+import Help from "./Help/Help"
 
-export { Scores, Board, Tile }
+export { Scores, Board, Tile, Help }

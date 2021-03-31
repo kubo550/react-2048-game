2d63b3a0cf416528f0d4b557eb693e297cf252f4
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-01 00:04:04 +0200

    Create styles to Help comp

---
+++ src/components/Help/Help.style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    width: 530px;
+    margin: 20px auto;
+
+    p {
+        color: #776e65;   
+        font-size: 1.2rem;
+    }
+
+    a {
+        color: #776e65;
+        font-size: 1.5rem;  
+    }
+`

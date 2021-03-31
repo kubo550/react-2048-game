24dc1f875cb2e138ac9a959e19c1eb6f04bbd601
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-03-31 23:15:42 +0200

    Create Board Score

---
+++ src/components/Scores/Scores.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState, useCallback } from "react";
+import * as S from "./Scores.style";
+
+interface ScoresProps {
+  score: number;
+  onClick: () => void;
+}
+
+const Scores: React.FC<ScoresProps> = ({ score, onClick }) => {
+  const [bestScore, setBestScore] = useState(0);
+
+  const checkBestScore = useCallback(
+    () => setBestScore(Math.max(score, bestScore)),
+    [score, bestScore]
+  );
+
+  useEffect(() => checkBestScore(), [checkBestScore]);
+  return (
+    <S.ScoresBoard>
+      <S.Header>
+        <h1>2048</h1>
+        <S.ScoreWrapper>
+          <S.ScoreContainer>
+            <span>Score</span>
+            <p> {score} </p>
+          </S.ScoreContainer>
+
+          <S.ScoreContainer>
+            <span>Best</span>
+            <p> {bestScore} </p>
+          </S.ScoreContainer>
+        </S.ScoreWrapper>
+      </S.Header>
+      <S.Info>
+        <p>Join the tiles, get to 2048!</p>
+        <button onClick={onClick}>New Game </button>
+      </S.Info>
+    </S.ScoresBoard>
+  );
+};
+
+export default Scores;

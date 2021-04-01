5435948be46618087629f9a39390a98ad8a9a078
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-02 00:11:02 +0200

    Add animation to Tile

--- src/components/Board/Tile/Tile.styles.ts
+++ src/components/Board/Tile/Tile.styles.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { colors } from "./Tile.color";
+import { motion } from "framer-motion";
 
 interface TileProps {
     value: number
@@ -8,14 +9,15 @@ interface TileProps {
 const HIGTER_BG = "#3C3A32";
 const DEFAULT_BG = "#CDC1B4";
 
-export const Tile = styled.div<TileProps>`
+export const Tile = styled(motion.div) <TileProps>`
     width: 100%;
+    max-width: 100%;
     height: 100%;
     background-color: ${({ value }) => value ? colors.get(value) || HIGTER_BG : DEFAULT_BG};
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 64px;
+    font-size: ${({ value }) => value > 1000 ? "36px" : value > 100 ? "42px" : "50px"};
     font-weight: 700;
     color: ${({ value }) => value <= 4 ? "#776e65" : "#F9F6F2"};
 `;
--- src/components/Board/Tile/Tile.tsx
+++ src/components/Board/Tile/Tile.tsx
@@ -5,8 +5,10 @@ interface TileProps {
   value: number;
 }
 
-const Tile: FC<TileProps> = ({ value }) => {
-  return <S.Tile value={value}> {value || ""} </S.Tile>;
-};
+const Tile: FC<TileProps> = ({ value }) => (
+  <S.Tile initial={{ opacity: 0 }} animate={{ opacity: 1 }} value={value}>
+    {value || ""}
+  </S.Tile>
+);
 
 export default Tile;

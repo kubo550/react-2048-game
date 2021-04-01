7393d5c9108fae7989dd1cb817fbf8aebedb26a4
kubo550 <43968748+kubo550@users.noreply.github.com>
2021-04-02 00:07:15 +0200

    Add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import { Board, Help } from "./components";
+import { About, Board, Help } from "./components";
+import styled from "styled-components";
 
-const App = () => {
-  return (
-    <>
-      <Board />
-      <hr />
-      <Help />
-      <hr />
-    </>
-  );
-};
+const Screen = styled.div`
+  width: 430px;
+  min-height: 100vh;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const App = () => (
+  // Todo Create own HR
+  <Screen>
+    <Board />
+    <hr />
+    <Help />
+    <hr />
+    <About />
+  </Screen>
+);
 
 export default App;
